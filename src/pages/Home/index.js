7c6eee3f31801f8ea81644580c9bb4644081f9dd
Mateusinhodev <mateusdev23@gmail.com>
2025-05-07 13:18:31 -0300

    Craindo e estilizando a tela de login

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,50 @@
+import {useState } from 'react';
+import './home.css'
+
+import { Link } from 'react-router-dom';
+
 export default function Home() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function handleLogin(e) {
+      e.preventDefault(); // Previne o comportamento padrão de atualizar a página
+
+      if(email !== '' && senha !== '') {
+        alert("Teste")
+      } else {
+        alert("Prencha todos os campos")
+      }
+    }
+
     return (
-      <div>
-        <h1>Home</h1>
+      <div className='home-container'>
+        <h1>Lista de tarefas</h1>
+        <span>Gerencie sua agenda de forma fácil.</span>
+
+        <form className='form' onSubmit={handleLogin}>
+          <input
+            type='text'
+            placeholder="Digite seu email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            autoComplete={false}
+            type='password'
+            placeholder="******"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link className='button-link' to={"/register"}>
+          Não possui uma conta? Cadastre-se
+        </Link>
       </div>
     );
 }
