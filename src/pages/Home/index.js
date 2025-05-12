3661e85f99fecd554d72288b6729c55dad0b61a4
Mateusinhodev <mateusdev23@gmail.com>
2025-05-12 13:09:47 -0300

    Buscando, listando e concluido tarefa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,17 +4,41 @@ import './home.css'
 import { auth, db } from '../../firebaseConnection';
 import { signOut } from 'firebase/auth';
 
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, query, orderBy, where, snapshotEqual, doc, deleteDoc } from 'firebase/firestore';
 
 export default function Home() {
 
     const [tarefaInput, setTarefaInput] = useState('');
     const [user, setUser] = useState({})
 
+    const [tarefas, setTarefas] = useState([])
+
     useEffect(() => {
         async function loadTarefas() {
-            const userDatail = localStorage.getItem("@detailUser")
-            setUser(JSON.parse(userDatail))
+            const userDetail = localStorage.getItem("@detailUser")
+            setUser(JSON.parse(userDetail))
+
+             if(userDetail) {
+                const data = JSON.parse(userDetail)
+
+                const tarefaRef = collection(db, "tarefas")
+                const q = query(tarefaRef, orderBy("created", "desc"), where("userUid", "==", data?.uid))
+                const unsub = onSnapshot(q, (snapshot) => {
+                    let lista = [];
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            tarefa: doc.data().tarefa,
+                            userUid: doc.data().userUid
+
+                        })
+                    })
+
+                    console.log(lista);
+                    setTarefas(lista);
+                })
+            }
         }
 
         loadTarefas()
@@ -29,7 +53,7 @@ export default function Home() {
         }
 
         await addDoc(collection(db, "tarefas"), {
-            tarefas: tarefaInput,
+            tarefa: tarefaInput,
             created: new Date(),
             userUid: user?.uid
         })
@@ -47,6 +71,11 @@ export default function Home() {
         await signOut(auth);
     }
 
+    async function deleteTarefa(id) {
+        const docRef = doc(db, "tarefas", id)
+        await deleteDoc(docRef)
+    }
+
     return(
         <div className='home-container'>
             <h1>Minhas tarefas</h1>
@@ -61,14 +90,16 @@ export default function Home() {
                 <button className='btn-register' type='submit'>Registrar tarefa</button>
             </form>
 
-            <article className='list'>
-                <p>Estudar Javascript e React</p>
+            {tarefas.map((item) => (
+                <article className='list'>
+                    <p>{item.tarefa}</p>
 
-                <div>
-                    <button>Editar</button>
-                    <button className='btn-delete'>Concluir</button>
-                </div>
-            </article>
+                    <div>
+                        <button>Editar</button>
+                        <button className='btn-delete' onClick={() => deleteTarefa(item.id)}>Concluir</button>
+                    </div>
+                </article>
+            ))}
 
             <button className='btn-logout' onClick={handleLogout}>Sair</button>
         </div>
